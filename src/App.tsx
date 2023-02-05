d6b08553add223f42994bf2927ebc717fa0671f4
Andrii-dan <89802969+Andrii-dan@users.noreply.github.com>
2023-02-05 05:57:48 +0100

    theme added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import { CssBaseline } from '@mui/material';
+import { useState } from 'react';
+import { Button, CssBaseline, ThemeProvider } from '@mui/material';
+import dark from './themes/dark';
+import light from './themes/light';
+
 import './App.scss';
 
 function App() {
+	const [theme, setTheme] = useState(light);
+
+	const changeTheme = () => {
+		theme === light ? setTheme(dark) : setTheme(light);
+	};
+
 	return (
 		<>
-			<CssBaseline />
-			<div>Hello Football World!</div>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<div style={{ height: '100vh', width: '100%' }}>
+					Hello Football World!
+					<Button variant='contained' color='primary' onClick={changeTheme}>
+						Change Theme
+					</Button>
+				</div>
+			</ThemeProvider>
 		</>
 	);
 }
